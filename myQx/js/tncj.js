d6f33545a9d39575c82b4a3a75428a9df507dac8
pq <pq>
2020-02-07 13:07:34 +0800

    energy

--- myQx/js/tncj.js
+++ myQx/js/tncj.js
@@ -51,8 +51,9 @@ class TNCJ {
   energy() {
     const res = config.decrypt(config.resBody)
     console.log(res)
-    // res["data"]["energy"] = 10
-    res["data"]["nextEnergyAt"] = res["data"]["sysTime"] + 1
+    res["data"]["energy"] = 5
+    res["data"]["lastEnergyAt"] = res["data"]["sysTime"] - 5
+    res["data"]["nextEnergyAt"] = res["data"]["sysTime"] + 5
     config.done(config.encrypt(res))
   }
 
