b962be00a3df016da73f9b5d6fdc273e565fc984
pq <pq@miuetech>
2020-03-05 14:34:38 +0800

    update 91short.js

--- myQx/js/91short.js
+++ myQx/js/91short.js
@@ -1,4 +1,3 @@
-let path = $request.path 
-path = path.replace(/&duration=\d+/g, "")
-console.log(path)
-$done({path: path});
+let path = $request.path
+path = path.replace(/duration=(\d+)/, "duration=300")
+$done({ path: path });
