25df1eb80bcf56bfabbb0dc0ec015a0ba0d9bda7
LeeeMooo <767104491@qq.com>
2020-07-29 22:00:44 +0800

    Update mktools.js

--- myQx/js/mktools.js
+++ myQx/js/mktools.js
@@ -1,14 +1,12 @@
-// ==FeHelperMonkey==
-// @reminder        请不要删除这部分代码注释，这是FeHelper油猴脚本能正常工作的基本条件！当然，你可以按需修改这里的内容！
-// @id              mf_1596020101710
-// @name            mikutool
-// @url-pattern     https://tools.miku.ac/*
-// @enable          true
-// @require-js      
-// @auto-refresh    0
-// @updated         2020-07-29 19:25:14
-// ==/FeHelperMonkey==
+// ==UserScript==
+// @name         mikutools
+// @namespace    mikutools-pq
+// @match        http*://tools.miku.ac/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
 
+'use strict';
 
 (() => {
     let dom = document.querySelector('#__nuxt');
