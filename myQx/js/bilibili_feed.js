00cd3d27f202c24c94899fdef0e3ece5497c42f8
LeeeMooo <pq@mjj.edu.hk>
2022-04-02 10:56:56 +0800

    Delete bilibili_feed.js

--- myQx/js/bilibili_feed.js
+++
@@ -1,9 +0,0 @@
-let body = $response.body
-body=JSON.parse(body)
-const items = body['data']['items']
-const filterItems = items.filter(v =>{
-    return !v['card_goto'].includes('ad')
-})
-const newBody = Object.assign({}, body, {data: {items: filterItems}})
-body = JSON.stringify(newBody)
-$done(body)
